ab250baf4828c520e9ecd8543865cbcecc1ce205
zhuoshen617@gmail.com <joe@Joes-iMac.local>
2021-04-14 22:00:36 -0400

    abc

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 // console.log(process.env.SECRET);
-iabc
+
 const express = require("express");
 const app = express();
 const path = require ("path");
@@ -29,21 +29,26 @@ app.use(express.urlencoded ({extended:true}));
 app.use(express.static(__dirname));
 
 
+function getRankings(prevStockList, currStockList) {
+  // finish this getRankings here
+  let rankingChanges = [];
+  rankingChanges.push({"ticker": "GME", "change": 0});
+  rankingChanges.push({"ticker": "UBER", "change": -1});
+  rankingChanges.push({"ticker": "PLTR", "change": 2});
+  rankingChanges.push({"ticker": "VUG"});
+  return rankingChanges;
+}
+
 app.get("/", async (req, res) => {
   const latest = await Stock.find().sort({timeStamp:-1}).limit(1);
   const latestTimeStamp = latest[0].timeStamp;
   const stockList  = await Stock.find({timeStamp: latestTimeStamp}).exec();
   const prev = await Stock.find({timeStamp: {$lte: latestTimeStamp - 2*60*60*1000}}).sort({timeStamp:-1}).limit(1);
-
-  console.log(`latest time: ${latestTimeStamp}`);
   const prevTimeStamp = prev[0].timeStamp;
-
-  console.log(`prev time: ${prevTimeStamp}`);
-
   const prevStockList = await Stock.find({timeStamp: prevTimeStamp}).exec();
 
-  
-  console.log(prevStockList);
+  console.log("currentStockList:", stockList);
+  console.log("prevStockList:", prevStockList);
 
   let obj = {};
   for (let i=0; i<stockList.length; i++) {
@@ -54,16 +59,10 @@ app.get("/", async (req, res) => {
     obj["previousClose"+i] = stockList[i].previousClose;
     obj["fiftyDayAverage"+i] = stockList[i].fiftyDayAverage;
     obj["averageDailyVolume10Day"+i] = stockList[i].averageDailyVolume10Day;
-
-    // obj["rankingChange"+i] =  
   }
-
-  obj["rankingChange0"] = 0;
-  obj["rankingChange1"] = -1;
-  obj["rankingChange2"] = 0;
-
+  obj["rankingChanges"] = getRankings(prevStockList, stockList);
+  
   res.render("index", obj);
-
 })
 
 const port = process.env.PORT || 8080;
