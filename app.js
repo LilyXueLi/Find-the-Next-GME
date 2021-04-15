8ece1c4d94c55034a20a46e121d3712d76b17ac9
zhuoshen617@gmail.com <joe@Joes-iMac.local>
2021-04-14 21:58:56 -0400

    abc

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 // console.log(process.env.SECRET);
-
+iabc
 const express = require("express");
 const app = express();
 const path = require ("path");
