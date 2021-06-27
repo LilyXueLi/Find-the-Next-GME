28e896a1ae506ae5c9c51164b7392f36ae6eb08a
Lily Li <lilydenris@gmail.com>
2021-06-27 16:49:12 -0400

    code clean-up

--- Scraper.py
+++ Scraper.py
@@ -1,14 +1,22 @@
 import time
 from datetime import datetime
 
-import praw
 import pymongo
 from pymongo import MongoClient
 
+import praw
+
 import yfinance as yf
+from dotenv import load_dotenv
+
+import dotenv
+import os
+
 
 def main():
-    client = MongoClient("mongodb+srv://first_user:JEfzAAafaC5mDH6l@cluster0.v5psh.mongodb.net/findMyGME?retryWrites=true&w=majority")
+    load_dotenv()
+
+    client = MongoClient(os.environ['DB_URL'])
     collection = client["findMyGME"]["stocks"]
 
     nasdaq_dict = {}
@@ -23,10 +31,6 @@ def main():
                 name = line.split("\t")[1]
                 nasdaq_dict[symbol] = name
                 ticker_dict[symbol] = name
-            # else:
-            #     print(f"empty line in nasdaq: {line}")
-
-        # print(nasdaq_dict)
 
     with open("Ticker Symbols/NYSE.txt", "r") as nyse:
         lines = [line.strip() for line in nyse.readlines()]
@@ -36,18 +40,11 @@ def main():
                 name = line.split("\t")[1]
                 nyse_dict[symbol] = name
                 if symbol not in ticker_dict:
-                    #     print(f"{symbol} already exists in NASDAQ")
-                    # else:
                     ticker_dict[symbol] = name
-            # else:
-            #     print(f"empty line in nyse: {line}")
 
-    reddit = praw.Reddit(client_id="3j4aMHa4CXXC7Q", client_secret="84T5rXSssKEm5H07XBIJ4wgo1MDdRw",
-                         user_agent="for testing")
+    reddit = praw.Reddit(client_id = os.environ['client_id'], client_secret = os.environ['client_secret'], user_agent = os.environ['user_agent'])
 
     hot_posts = reddit.subreddit("wallstreetbets").hot(limit=500)
-
-    # print(type(hot_posts))
 
     def add_to_dict(word_in_post, ticker_dict):
         for word in word_in_post:
@@ -55,27 +52,16 @@ def main():
             if clean_word in ticker_dict:
                 ticker_count.setdefault(clean_word, 0)
                 ticker_count[clean_word] += 1
-            # else:
-            #     print(f"{clean_word} not in ticker list")
 
     ticker_count = {}
-    # counter = 0
+
     for post in hot_posts:
-        # print(f"This is the {counter} post content - "
-        #       f"title: {post.title}"
-        #       f"content: {post.selftext}")
-        # counter +=1
         word_in_title = post.title.split()
         add_to_dict(word_in_title, ticker_dict)
         word_in_content = post.selftext.split()
         add_to_dict(word_in_content, ticker_dict)
 
-    # print(ticker_count)
-
     sorted_ticker_count = dict(sorted(ticker_count.items(), key=lambda item: item[1], reverse=True))
-
-    # print(sorted_ticker_count)
-    # print(f"Before clean {len(ticker_count)}")
 
     # A list of abbreviations generally do not represent tickers.
     black_list = ["A", "DD", "FOR", "CEO", "ALL", "EV", "OR", "AT", "RH", "ONE", "ARE", "VERY", "ON", "EDIT"]
@@ -86,19 +72,15 @@ def main():
         if key not in black_list:
             clean_sorted_ticker_dict[key] = value
 
-    # print(clean_sorted_ticker_dict)
-    # print(f"After clean {len(clean_sorted_ticker_dict)}")
-
     sorted_ticker_list = list(clean_sorted_ticker_dict.keys())
 
-    # top10_toDB = {}
     now = datetime.now()
     for i in range(10):
         output_ticker = sorted_ticker_list[i]
         output_count = clean_sorted_ticker_dict[output_ticker]
         stock_info = yf.Ticker(output_ticker).info
         toDB = {
-            "rank": i+1,
+            "rank": i + 1,
             "ticker": output_ticker,
             "name": ticker_dict[output_ticker],
             "industry": stock_info["industry"],
@@ -110,19 +92,8 @@ def main():
         }
         print(toDB)
         collection.insert_one(toDB)
-        # top10_toDB[str(i)] = (output_ticker, output_count)
-        # print(f"{output_ticker}: {output_count}")
-
-    # collection.insert_one(top10_toDB)
-
-    # print(top10_toDB)
-
-
 
 
 if __name__ == "__main__":
-    # while True:
     main()
-    # time_wait = 10
-    # time.sleep(time_wait * 60)
-    # print(f"Waiting {time_wait} minutes, now is {datetime.now()}")
+
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-// console.log(process.env.SECRET);
 
 const express = require("express");
 const app = express();
@@ -12,14 +11,13 @@ const Stock = require("./models/stock");
 const Count = require("./models/visitorCount");
 
 const dbUrl = process.env.DB_URL;
-console.log(dbUrl);
-//'mongodb://127.0.0.1:27017/fnGME'
+
 mongoose.connect(dbUrl, { useNewUrlParser: true }, { useUnifiedTopology: true })
     .then(() => {
-        console.log("MONGO CONNECTION OPEN!!! ")
+        console.log("MONGO CONNECTION OPEN")
     })
     .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!")
+        console.log("MONGO CONNECTION ERROR")
         console.log(err)
     })
 
@@ -31,7 +29,6 @@ app.use(express.static(__dirname));
 
 
 function getRankings(prevStockList, currStockList) {
-  // finish this getRankings here
   let rankingChanges = [];
   let prevPositions = [];
   let currInPrevPositions = [];
@@ -39,9 +36,6 @@ function getRankings(prevStockList, currStockList) {
   for (prevStock of prevStockList) {
     prevPositions.push(prevStock["ticker"]);
   }
-
-  // console.log(prevPositions);
-  // console.log("check ticker" + currStockList[0]["ticker"]);
 
   for (let i=0; i<10; i++) {
     currInPrevPositions.push(prevPositions.indexOf(currStockList[i]["ticker"])+1);
@@ -56,17 +50,13 @@ function getRankings(prevStockList, currStockList) {
     }
     
   }
-  // rankingChanges.push({"ticker": currStockList[0]["ticker"], "change": currStockList[0]});
-  // rankingChanges.push({"ticker": "UBER", "change": -1});
-  // rankingChanges.push({"ticker": "PLTR", "change": 2});
-  // rankingChanges.push({"ticker": "VUG"});
   return rankingChanges;
 }
 
 app.get("/", async (req, res) => {
   const latestCount = await Count.find().sort({_id: -1}).limit(1);
-  const visitorCount = latestCount[0].count +1; 
-  const filter = {count: visitorCount-1};
+  const visitorCount = latestCount[0].count + 1; 
+  const filter = {count: visitorCount - 1};
   const update = {count : visitorCount};
   let doc = await Count.findOneAndUpdate(filter, update, {upsert: true, sort: {created: -1}});
 
@@ -77,11 +67,8 @@ app.get("/", async (req, res) => {
   const prevTimeStamp = prev[0].timeStamp;
   const prevStockList = await Stock.find({timeStamp: prevTimeStamp}).exec();
 
-  console.log("currentStockList:", stockList);
-  console.log("prevStockList:", prevStockList);
-  console.log("visitorCount", visitorCount);
-
   let obj = {};
+
   for (let i=0; i<stockList.length; i++) {
     obj["stock"+i] = stockList[i].ticker;
     obj["name"+i] = stockList[i].name;
@@ -91,6 +78,7 @@ app.get("/", async (req, res) => {
     obj["fiftyDayAverage"+i] = stockList[i].fiftyDayAverage;
     obj["averageDailyVolume10Day"+i] = stockList[i].averageDailyVolume10Day;
   }
+
   obj["rankingChanges"] = getRankings(prevStockList, stockList);
   
   obj["visitorCount"] = visitorCount;
@@ -99,6 +87,7 @@ app.get("/", async (req, res) => {
 })
 
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log(`APP IS LISTENING ON PORT ${port}!`)
 })
